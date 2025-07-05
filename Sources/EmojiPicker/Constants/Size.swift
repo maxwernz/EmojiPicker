b472761102df986f708a1a83d788116e5868b28b
maxwernz <mawernz@outlook.de>
2025-07-05 14:55:39 +0200

    change animation duration

--- Sources/EmojiPicker/Constants/Size.swift
+++ Sources/EmojiPicker/Constants/Size.swift
@@ -2,7 +2,7 @@ import Foundation
 
 enum Size {
     static let backgroundOpacity = 0.1
-    static let animationDuration = 0.5
+    static let animationDuration = 0.3
     static let contentHeight = 390.0
     static let scrollHeight = 230.0
     static let emojiSize = 32.0
